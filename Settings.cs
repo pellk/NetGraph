1c0503b2d811034b176c83e20f94ee1ae4822d37
pellk <newroz@gmail.com>
2023-01-19 15:21:37 +0330

    settings

---
+++ Settings.cs
@@ -0,0 +1,52 @@
+using Avalonia;
+
+public class Settings
+{
+   private const char EqualChar = '=';
+   private static readonly string settingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.ini");
+   public static int SentMax { get; set; }
+   public static int ReceivedMax { get; set; }
+   public static PixelPoint Position { get; set; }
+   public static string CurrentNetwork { get; set; }
+
+   public static void Load()
+   {
+      int x = 0, y = 0;
+      foreach (string line in File.ReadAllLines(settingsPath))
+      {
+         int equals = line.IndexOf(EqualChar);
+         string label = line[..equals];
+         string value = line[(equals + 1)..];
+         switch (label)
+         {
+            case "SentMax":
+               SentMax = int.Parse(value);
+               break;
+            case "ReceivedMax":
+               ReceivedMax = int.Parse(value);
+               break;
+            case "CurrentNetwork":
+               CurrentNetwork = value;
+               break;
+            case "X":
+               x = int.Parse(value);
+               break;
+            case "Y":
+               y = int.Parse(value);
+               break;
+         }
+      }
+      Position = new PixelPoint(x, y);
+   }
+
+   public static void Save()
+   {
+      var lines = new List<string>();
+      lines.Add($"SentMax{EqualChar}{SentMax}");
+      lines.Add($"ReceivedMax{EqualChar}{ReceivedMax}");
+      lines.Add($"CurrentNetwork{EqualChar}{CurrentNetwork}");
+      lines.Add($"X{EqualChar}{Position.X}");
+      lines.Add($"Y{EqualChar}{Position.Y}");
+      File.WriteAllLines(settingsPath, lines);
+   }
+}
--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -1,97 +1,77 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
 using System.Net.NetworkInformation;
 
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace NetGraph
 {
-    public class SettingsWindow : Window
-    {
-        private Dictionary<string, string> Settings { get; set; }
-        private int SentMax { get; set; }
-        private int ReceivedMax { get; set; }
-        private int CurrentNetwork { get; set; }
-        private PixelPoint GraphPosition { get; set; }
-        private NetworkInterface[] Interfaces { get; set; }
-        private GraphWindow GraphWindow { get; set; }
-        private readonly string settingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.config");
+   public class SettingsWindow : Window
+   {
+      private int SentMax { get; set; }
+      private int ReceivedMax { get; set; }
+      private int CurrentNetwork { get; set; }
+      private NetworkInterface[] Interfaces { get; set; }
+      private GraphWindow GraphWindow { get; set; }
 
-        public SettingsWindow()
-        {
-            InitializeComponent();
+      public SettingsWindow()
+      {
+         InitializeComponent();
 
-            Settings = File.ReadAllLines(settingsPath)
-                .ToDictionary(
-                    s => s.Substring(0, s.IndexOf(':')),
-                    s => s[(s.IndexOf(':') + 1)..]);
+         Settings.Load();
 
-            SentMax = int.Parse(Settings["SentMax"]);
-            ReceivedMax = int.Parse(Settings["ReceivedMax"]);
+         SentMax = Settings.SentMax;
+         ReceivedMax = Settings.ReceivedMax;
 
-            if (!NetworkInterface.GetIsNetworkAvailable())
-                throw new NetworkInformationException();
+         if (!NetworkInterface.GetIsNetworkAvailable())
+            throw new NetworkInformationException();
 
-            Interfaces = NetworkInterface.GetAllNetworkInterfaces()
-                .Where(i => i.OperationalStatus == OperationalStatus.Up).ToArray();
+         Interfaces = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(i => i.OperationalStatus == OperationalStatus.Up).ToArray();
 
-            CurrentNetwork = 0;
-            for (int i = 0; i < Interfaces.Length; i++)
-                if (Interfaces[i].Id == Settings["CurrentNetwork"])
-                    CurrentNetwork = i;
-            GraphPosition = new PixelPoint(int.Parse(Settings["X"]), int.Parse(Settings["Y"]));
+         CurrentNetwork = 0;
+         for (int i = 0; i < Interfaces.Length; i++)
+            if (Interfaces[i].Id == Settings.CurrentNetwork)
+               CurrentNetwork = i;
 
-            this.DataContext = this;
-        }
+         this.DataContext = this;
+      }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+      private void InitializeComponent()
+      {
+         AvaloniaXamlLoader.Load(this);
+      }
 
-        private void OkButton_Click(object serder, RoutedEventArgs e)
-        {
-            Settings["CurrentNetwork"] = Interfaces[CurrentNetwork].Id;
-            Settings["SentMax"] = SentMax.ToString();
-            Settings["ReceivedMax"] = ReceivedMax.ToString();
-            File.WriteAllLines(settingsPath, Settings.Select(s => s.Key + ":" + s.Value));
+      private void OkButton_Click(object serder, RoutedEventArgs e)
+      {
+         Settings.CurrentNetwork = Interfaces[CurrentNetwork].Id;
+         Settings.SentMax = SentMax;
+         Settings.ReceivedMax = ReceivedMax;
+         Settings.Save();
 
-            var stat = Interfaces[CurrentNetwork].GetIPv4Statistics();
-            GraphWindow?.Close();
-            GraphWindow = new GraphWindow
-            {
-                LastTraffic = (Sent: stat.BytesSent, Received: stat.BytesReceived),
-                Network = Interfaces[CurrentNetwork],
-                SentMax = SentMax,
-                ReceivedMax = ReceivedMax
-            };
-            GraphWindow.PositionChanged += Graph_PositionChanged;
-            GraphWindow.Show();
-            GraphWindow.Position = GraphPosition;
-            WindowState = WindowState.Minimized;
-        }
+         var stat = Interfaces[CurrentNetwork].GetIPv4Statistics();
+         GraphWindow?.Close();
+         GraphWindow = new GraphWindow
+         {
+            LastTraffic = (Sent: stat.BytesSent, Received: stat.BytesReceived),
+            Network = Interfaces[CurrentNetwork],
+            SentMax = SentMax,
+            ReceivedMax = ReceivedMax,
+         };
+         GraphWindow.Show();
+         GraphWindow.Position = Settings.Position;
+         WindowState = WindowState.Minimized;
+      }
 
-        private void QuitButton_Click(object serder, RoutedEventArgs e)
-        {
-            Close();
-        }
+      private void QuitButton_Click(object serder, RoutedEventArgs e)
+      {
+         Close();
+      }
 
-        private void Graph_PositionChanged(object sender, PixelPointEventArgs e)
-        {
-            Settings["X"] = e.Point.X.ToString();
-            Settings["Y"] = e.Point.Y.ToString();
-            File.WriteAllLines(settingsPath, Settings.Select(s => s.Key + ":" + s.Value));
-        }
-
-        private void Window_Closing(object sender, CancelEventArgs e)
-        {
-            GraphWindow?.Close();
-        }
-    }
+      private void Window_Closing(object sender, CancelEventArgs e)
+      {
+         GraphWindow?.Close();
+      }
+   }
 }
